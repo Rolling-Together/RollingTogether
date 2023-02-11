b8146e77ba85cd0b504a0278af7f34090964e411
imiswest <ies9961@naver.com>
2023-02-12 01:26:20 +0900

    es - 스크린 생성 완료

---
+++ lib/ui/screens/post_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// 7. 게시글
+
+class PostScreen extends StatelessWidget {
+  const PostScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/ui/screens/store_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// 7. 식당, 카페 정보화면
+
+class StoreScreen extends StatelessWidget {
+  const StoreScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
