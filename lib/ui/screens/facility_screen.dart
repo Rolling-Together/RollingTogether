7656cbab96305db77f3da81c4bbe5f9c3b8f8711
이홍주 <hongju4385@gmail.com>
2023-02-12 14:24:29 +0900

    initial setting

---
+++ lib/ui/screens/facility_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FacilityScreen extends StatelessWidget {
+  const FacilityScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/ui/screens/trans_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TransScreen extends StatelessWidget {
+  const TransScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
