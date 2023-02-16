8618ec9867afc678a25829ee407622c6766938d1
이홍주 <hongju4385@gmail.com>
2023-02-16 17:07:03 +0900

    게시글 textField 추가

--- lib/ui/screens/location_screen.dart
+++ lib/ui/screens/location_screen.dart
@@ -8,44 +8,65 @@ class LocationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            ///대분류
-            padding: EdgeInsets.only(
-                top: MediaQuery.of(context).size.width * 0.1,
-                left: MediaQuery.of(context).size.width * 0.05),
-            alignment: Alignment.centerLeft,
-            child: Text('위험 장소', style: TextStyle(fontSize: 16)),
-          ),
-          Container(
+      body: SingleChildScrollView( ///textfield가 늘어남에 따라 스크롤 가능하게
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                ///대분류
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.width * 0.1,
+                    left: MediaQuery.of(context).size.width * 0.05),
+                alignment: Alignment.centerLeft,
+                child: Text('위험 장소', style: TextStyle(fontSize: 16)),
+              ),
+              Container(
 
-              ///카테고리
-              padding: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.01,
-                  bottom: MediaQuery.of(context).size.height * 0.03),
-              alignment: Alignment.center,
-              width: MediaQuery.of(context).size.width * 0.9,
-              decoration: BoxDecoration(),
-              child: CategoryButton()),
-          ImageUploader(),
-          Container(
-            padding: EdgeInsets.all(MediaQuery.of(context).size.width*0.2),
-            child: Text('지도 API'),
+                  ///카테고리
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.01,
+                      bottom: MediaQuery.of(context).size.height * 0.03),
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  decoration: BoxDecoration(),
+                  child: CategoryButton()),
+              ImageUploader(),
+              Container(
+                padding:
+                    EdgeInsets.all(MediaQuery.of(context).size.width * 0.2),
+                child: Text('지도 API'),
+              ),
+              Container(
+                ///주소
+                padding: EdgeInsets.only(
+                    left: MediaQuery.of(context).size.width * 0.05),
+                alignment: Alignment.centerLeft,
+                child: Text('주소'),
+              ),
+              Container(
+                ///주소 불러오는 값 임의로 지정해놨음
+                padding: EdgeInsets.only(
+                    left: MediaQuery.of(context).size.width * 0.05,
+                    top: MediaQuery.of(context).size.height * 0.01),
+                alignment: Alignment.centerLeft,
+                child: Text('부산광역시 남구 용소로 45, 부경대학교 대연캠퍼스'),
+              ),
+              Container(
+                margin: EdgeInsets.all(MediaQuery.of(context).size.width*0.05),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black)
+                ),
+                child : TextField(
+                  decoration: InputDecoration(
+                    focusedBorder: InputBorder.none
+                  ),
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+              ),
+              ),
+            ],
           ),
-          Container( ///주소
-            padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.05),
-            alignment: Alignment.centerLeft,
-            child: Text('주소'),
-          ),
-          Container( ///주소 불러오는 값 임의로 지정해놨음
-            padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.05,
-            top : MediaQuery.of(context).size.height*0.01),
-            alignment: Alignment.centerLeft,
-            child: Text('부산광역시 남구 용소로 45, 부경대학교 대연캠퍼스'),
-          ),
-          TextFormField(),
-        ],
+        ),
       ),
     );
   }
@@ -108,30 +129,31 @@ class _ImageUploaderState extends State<ImageUploader> {
   File? _image;
   final picker = ImagePicker();
 
-  ///비동기 처리 > 갤러리에서 이미지 가져오기
+  ///비동기 처리 > 이미지 가져오기
   Future<void> _pickImage(ImageSource source) async {
     final pickedFile = await picker.pickImage(source: source);
-    if (pickedFile != Null) { ///if없어도 될 듯. 어차피 !.로 null이 아님을 정해줘서
-    setState(() {
-      _image = File(pickedFile!.path);
-    });
+    if (pickedFile != Null) {
+      ///if없어도 될 듯. 어차피 !.로 null이 아님을 정해줘서
+      setState(() {
+        _image = File(pickedFile!.path);
+      });
     }
   }
 
-  Widget _optionDialog(){
+  Widget _optionDialog() {
     return AlertDialog(
       content: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           FloatingActionButton(
-            child: Icon(Icons.add_a_photo),
-              onPressed: (){
-              Navigator.of(context).pop();
-            _pickImage(ImageSource.camera);
-          }),
+              child: Icon(Icons.add_a_photo),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _pickImage(ImageSource.camera);
+              }),
           FloatingActionButton(
               child: Icon(Icons.wallpaper),
-              onPressed: (){
+              onPressed: () {
                 Navigator.of(context).pop();
                 _pickImage(ImageSource.gallery);
               }),
@@ -150,7 +172,8 @@ class _ImageUploaderState extends State<ImageUploader> {
       ),
       child: _image == null
           ? InkWell(
-              onTap: () => showDialog(context: context, builder: (_) => _optionDialog()),
+              onTap: () =>
+                  showDialog(context: context, builder: (_) => _optionDialog()),
               child: Icon(Icons.camera_alt),
             )
           : Image.file(_image!),
