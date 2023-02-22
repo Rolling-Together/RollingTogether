c9befe9f59ec11ad6e3485128e5e05ca7948fbe4
imiswest <ies9961@naver.com>
2023-02-23 01:13:01 +0900

    community screen 기초 구성

--- lib/ui/screens/community_screen.dart
+++ lib/ui/screens/community_screen.dart
@@ -1,12 +1,85 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class CommunityScreen extends StatelessWidget {
-  const CommunityScreen({Key? key}) : super(key: key);
+class CommunityScreen extends StatefulWidget {
+  _CommunityScreenState createState() => _CommunityScreenState();
+}
+
+class _CommunityScreenState extends State<CommunityScreen> {
+  String selectedCategory = "전체";
+
+  void selectCategory(String category) {
+    setState(() {
+      selectedCategory = category;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('CommunityScreen 2'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("커뮤니티"),
+      ),
+      body: Column(
+        children: [
+          // 카테고리 버튼들
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              OutlinedButton(
+                onPressed: () => selectCategory("전체"),
+                child: Text(
+                  "전체",
+                  style: TextStyle(
+                    color: selectedCategory == "전체" ? Colors.blue : null,
+                  ),
+                ),
+              ),
+              OutlinedButton(
+                onPressed: () => selectCategory("위험 장소"),
+                child: Text(
+                  "위험 장소",
+                  style: TextStyle(
+                    color: selectedCategory == "위험 장소" ? Colors.blue : null,
+                  ),
+                ),
+              ),
+              OutlinedButton(
+                onPressed: () => selectCategory("음식점/카페"),
+                child: Text(
+                  "음식점/카페",
+                  style: TextStyle(
+                    color: selectedCategory == "음식점/카페" ? Colors.blue : null,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          // 선택된 카테고리에 따라 다른 리스트 보여주기
+          Expanded(
+            child: ListView.builder(
+                itemCount: 10, // 리스트 개수는 임의로 설정
+                itemBuilder: (context, index) {
+                  if (selectedCategory == "전체") {
+                    return ListTile(
+                      title: Text("전체 리스트 $index"),
+                    );
+                  } else if (selectedCategory == "위험 장소") {
+                    return ListTile(
+                      title: Text("위험 장소 리스트 $index"),
+                    );
+                  } else if (selectedCategory == "음식점/카페") {
+                    return ListTile(
+                      title: Text("음식점/카페 리스트 $index"),
+                    );
+                  }
+                  return Container();
+                }
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
+
