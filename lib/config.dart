136728d7ea283fa6425331caa72900d6187b5023
JSPark <naversovc@gmail.com>
2023-02-28 14:57:38 +0900

    #12 위경도 -> 주소 반환 로직 개발

---
+++ lib/config.dart
@@ -0,0 +1,1 @@
+const kakaoRestApiKey = '4b3f22714643404dc99f7e430ec86967';
--- lib/data/remote/dangerous_zone/likes/models/likes_dangerous_zone.dart
+++ lib/data/remote/dangerous_zone/likes/models/likes_dangerous_zone.dart
@@ -35,6 +35,6 @@ class LikesDangerousZoneDto {
     id = docId;
     dangerousZoneDocId = map?['dangerousZoneDocId'];
     likes = map?['likes'];
-    latlng = map['latlng'];
+    latlng = map?['latlng'];
   }
 }
