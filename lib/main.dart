717da6fa5af5b6c807d69b542200c0ff91cb03b5
이홍주 <hongju4385@gmail.com>
2023-03-15 20:14:28 +0900

    ui/#40/ main.dart 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:rolling_together/ui/screens/0_login_screen.dart';
+import 'package:rolling_together/ui/screens/7_facility_post_screen.dart';
 import 'firebase_options.dart';
 import 'ui/screens/init_map_screen.dart';
 
@@ -11,8 +11,8 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp( GetMaterialApp(
+  runApp(const GetMaterialApp(
     debugShowCheckedModeBanner: false,
-    home: LoginScreen(),
+    home: FacilityScreen(),
   ));
 }
